2002d2e181ba567ab7bcb87396043a0f5ae0dc5b
Ángel <keisial@users.noreply.github.com>
2020-07-02 01:39:49 +0200

    Count concurrent allocations
    For sandmark.umz:
     concurrent: 15410
     maxconcurrent: 47262

--- um_malloc.c
+++ um_malloc.c
@@ -10,14 +10,16 @@ int memstats = 0;
 static struct {
 	int allocations, reallocations, duplications, frees;
 	uint32_t maxsize;
+	int concurrent, maxconcurrent;
 } statistics;
 
-#define HIT(x) do { if (__builtin_expect(memstats, 0)) { statistics.x++; } } while (0)
+#define HIT_N(x,c) do { if (__builtin_expect(memstats, 0)) { statistics.x += c; } } while (0)
+#define HIT(x) HIT_N(x,1)
 #define MEMDEBUG(x, ...) do { if (__builtin_expect(memdebug, 0)) printf(x, __VA_ARGS__); } while (0)
 
 platter um_alloc(platter size) {
 	platter p = { .ptr = calloc(size.v, 4) };
-	HIT(allocations);
+	HIT(allocations); HIT(concurrent); if (memstats && statistics.concurrent > statistics.maxconcurrent) statistics.maxconcurrent = statistics.concurrent;
 	if (memstats && size.v > statistics.maxsize) statistics.maxsize = size.v;
 
 	MEMDEBUG("alloc(%u) = %p\n", size.v, p.ptr);
@@ -38,7 +40,7 @@ uint32_t um_alloc_size(platter p) {
 
 platter um_duplicate(platter old) {
 	platter new;
-	HIT(duplications);
+	HIT(duplications); HIT(concurrent);
 	size_t size = um_alloc_size(old) * 4;
 	new.ptr = malloc(size);
 	memcpy(new.ptr, old.ptr, size);
@@ -49,6 +51,7 @@ platter um_duplicate(platter old) {
 
 void um_free(platter ptr) {
 	HIT(frees);
+	HIT_N(concurrent, -1);
 	free(ptr.ptr);
 
 	MEMDEBUG("free(%p)\n", ptr.ptr);
@@ -63,4 +66,6 @@ void um_memory_stats() {
 	STAT(duplications);
 	STAT(frees);
 	STAT(maxsize);
+	STAT(concurrent);
+	STAT(maxconcurrent);
 }
